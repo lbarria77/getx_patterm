2d3d0e024a9b6028f9f0295313002a0edde14e8a
Luis Barria <wahoosushibar@gmail.com>
2021-09-13 10:25:37 -0500

    Obtener Path de la imagen

--- lib/app/modules/home/widgets/movie_items.dart
+++ lib/app/modules/home/widgets/movie_items.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_patterm_demo/app/data/models/movies.dart';
+import 'package:get_patterm_demo/app/utils/constants.dart';
 
 class MovieItems extends StatelessWidget {
   final Movie movie;
@@ -12,6 +13,7 @@ class MovieItems extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: ListTile(
+        leading: Image.network("${Constants.THE_MOVIE_DB_IMG_PATH}${movie.posterPath}"),
         title: Text(movie.title),
         subtitle: Text(movie.overview),
       ),
--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -1,3 +1,4 @@
 class Constants{
   static const THE_MOVIE_DB_API_KEY = 'a5d3948189246769a1b3a851a9511fb4';
+  static const THE_MOVIE_DB_IMG_PATH = 'https://www.themoviedb.org/t/p/original';
 }
